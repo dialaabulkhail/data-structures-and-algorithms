a73968f965daa345b6a11de09d7d6d823dfa117f
dialaabulkhail <diala.sh.98@gmail.com>
2022-03-10 22:26:06 +0300

    first commit

---
+++ linked-list/linked_list/linked_list.py
@@ -0,0 +1,21 @@
+from configparser import NoSectionError
+from linked_list.node import Node
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    
+    
+    def insert(self, value):
+        return "Nothing"
+
+
+
+    def includes(self, value, boolean):
+        pass
+
+    
+
+    def to_string():
+        return "{ a } -> { b } -> { c } -> NULL"
